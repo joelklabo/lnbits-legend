b850601957a1a80388d35bdae89c80fe5f7334a3
Joel Klabo <joel@klabo.world>
2026-03-10 15:58:15 -0700

    Add support for BOLT12 offers

--- lnbits/bolt11.py
+++ lnbits/bolt11.py
@@ -1,7 +1,7 @@
-from bolt11 import (
+from bolt12 import (
     Bolt11 as Invoice,  # noqa: F401
 )
-from bolt11 import (
+from bolt12 import (
     decode,  # noqa: F401
     encode,  # noqa: F401
 )
